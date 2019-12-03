c0d1144b1c22e885782ab9af05ec1c06f81ce1e2
LPF <hiliupengfei@sina.com>
2019-12-03 17:22:00 +0800

    添加后台的文件

---
+++ 源码/后台/ylshop/src/cn/pengfei/ssm/mapper/tbUserMapper.java
@@ -0,0 +1,15 @@
+package cn.pengfei.ssm.mapper;
+
+import cn.pengfei.ssm.po.tbUsers;
+
+public interface tbUserMapper {
+	/**
+	 * 添加用户
+	 * 编写人：刘朋飞
+	 * 编写时间：2019年12月3日
+	 * @param user 用户实体
+	 * @return 返回执行后结果
+	 * @throws Exception 抛出异常
+	 */
+	public int insertUser(tbUsers user) throws Exception ;
+}
